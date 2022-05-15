56e6c558c585b7ff56de7043209bd4d4855ae6f0
zhanglingfei <419881361@qq.com>
2022-05-15 20:04:12 +0800

    add springboot

---
+++ src/main/java/com/zlf/main/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.zlf.main.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhanglf
+ * @Date 2022/5/15 7:17 上午
+ */
+@RestController()
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
+}
