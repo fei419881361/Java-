9b2b006b805869fb6904b275e2eabd1628bb248b
zhanglingfei <419881361@qq.com>
2022-05-15 23:07:45 +0800

    add springboot

---
+++ src/main/java/com/zlf/main/MainApplication.java
@@ -0,0 +1,15 @@
+package com.zlf.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanglf
+ * @Date 2022/5/15 7:10 上午
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
